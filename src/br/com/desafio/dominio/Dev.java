0bb07a53488721cee28abe254486d0155b40587b
V1n1c1u5P4iva <paivavinicius214@gmail.com>
2024-09-21 19:37:11 -0300

    Classes Dev e Bootcamp

---
+++ src/br/com/desafio/dominio/Dev.java
@@ -0,0 +1,84 @@
+package br.com.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Dev {
+
+    private String nome;
+    private Set<Conteudo> conteudoInscrito = new LinkedHashSet<>();
+    private Set<Conteudo> conteudoConcluido = new LinkedHashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+    };
+
+    public void progredir() {
+    };
+
+    public void calcularXp() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudoInscrito() {
+        return conteudoInscrito;
+    }
+
+    public void setConteudoInscrito(Set<Conteudo> conteudoInscrito) {
+        this.conteudoInscrito = conteudoInscrito;
+    }
+
+    public Set<Conteudo> getConteudoConcluido() {
+        return conteudoConcluido;
+    }
+
+    public void setConteudoConcluido(Set<Conteudo> conteudoConcluido) {
+        this.conteudoConcluido = conteudoConcluido;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((conteudoInscrito == null) ? 0 : conteudoInscrito.hashCode());
+        result = prime * result + ((conteudoConcluido == null) ? 0 : conteudoConcluido.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Dev other = (Dev) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (conteudoInscrito == null) {
+            if (other.conteudoInscrito != null)
+                return false;
+        } else if (!conteudoInscrito.equals(other.conteudoInscrito))
+            return false;
+        if (conteudoConcluido == null) {
+            if (other.conteudoConcluido != null)
+                return false;
+        } else if (!conteudoConcluido.equals(other.conteudoConcluido))
+            return false;
+        return true;
+    };
+
+
+    
+}
